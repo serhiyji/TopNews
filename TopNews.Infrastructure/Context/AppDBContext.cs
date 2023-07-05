281e7538eaa405c8b8b7afb259993ed5f9ef00e3
serhiyji <serhiy22823@gmail.com>
2023-07-05 22:14:19 +0300

    Add project files.

---
+++ TopNews.Infrastructure/Context/AppDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopNews.Infrastructure.Context
+{
+    internal class AppDBContext : IdentityDbContext
+    {
+        public AppDBContext() : base() { }
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options) { }
+    }
+}
