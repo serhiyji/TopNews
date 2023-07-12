adf4e7037a9c60921396d19bdd0de6d0bd6878c5
serhiyji <serhiy22823@gmail.com>
2023-07-12 18:35:41 +0300

    Start with sign in

--- TopNews.Web/Controllers/DashboardController.cs
+++ TopNews.Web/Controllers/DashboardController.cs
@@ -1,13 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace TopNews.Web.Controllers
 {
+    [Authorize]
     public class DashboardController : Controller
     {
         public IActionResult Index()
         {
             return View();
         }
+
+        [AllowAnonymous]
         public IActionResult SignIn()
         {
             return View();
