170048d93baa90db3cd52a818c37cf6ab2ed117d
serhiyji <serhiy22823@gmail.com>
2023-07-12 19:27:39 +0300

    Validation result

---
+++ TopNews.Core/Validation/User/LoginUserValidation.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TopNews.Core.DTOs.User;
+
+namespace TopNews.Core.Validation.User
+{
+    public class LoginUserValidation : AbstractValidator<UserLoginDTO>
+    {
+        public LoginUserValidation()
+        {
+            RuleFor(r => r.Email).NotEmpty().WithMessage("Field must not be empty").EmailAddress().WithMessage("Invalid email format");
+            RuleFor(r => r.Password).NotEmpty().WithMessage("Field must not be empty")
+                .MinimumLength(6).WithMessage("Password must be at least 6 charters")
+                .MaximumLength(128);
+            
+        }
+    }
+}
--- TopNews.Web/Controllers/DashboardController.cs
+++ TopNews.Web/Controllers/DashboardController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using FluentValidation.Results;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using TopNews.Core.Validation.User;
 
 namespace TopNews.Web.Controllers
 {
@@ -11,10 +13,25 @@ namespace TopNews.Web.Controllers
             return View();
         }
 
-        [AllowAnonymous]
+        [AllowAnonymous] // GET
         public IActionResult SignIn()
         {
             return View();
+        }
+
+        [AllowAnonymous] // HOST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult SignIn(TopNews.Core.DTOs.User.UserLoginDTO model)
+        {
+            LoginUserValidation validator = new LoginUserValidation();
+            ValidationResult valationReslt = validator.Validate(model);
+            if (valationReslt.IsValid) 
+            { 
+                return View();
+            }
+            ViewBag.AuthError = valationReslt.Errors[0];
+            return View(model); 
         }
     }
 }
